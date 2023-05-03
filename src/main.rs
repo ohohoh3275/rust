08348ec45f800d69b36865fa79c676fb731dbb97
ohjiyeon3275 <ojy940716@gmail.com>
2023-05-03 22:58:34 +0900

    reference counting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+
 fn take_string(s:String) {
 
 }
@@ -6,10 +8,20 @@ fn take_string(s:String) {
 fn main() {
     println!("Hello, world!");
 
-    let mut st = String::from("")
+    let mut st = String::from("");
 
+    // s == owner
+    // one-owner-at-a-time
     let s: String = String::from("Oh");
 
+    //let s2 = s;
+    //println!("{}", s);
+
+    let n1 = Rc::new(String::from("i am rc string which is different from just String"));
+    let n2= Rc::clone(&n1);
+    println!("{}", n1);
+    println!("{}", n2);
+
     let x="kkj";
-]
+
 }
