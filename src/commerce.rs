ffba030ec8bdf7d28ee3443bf215a6e79e957176
ohjiyeon3275 <ojy940716@gmail.com>
2023-05-12 22:14:32 +0900

    weak reference

--- src/commerce.rs
+++ src/commerce.rs
@@ -1,10 +1,11 @@
 struct Item {
     name:String,
     price:i64,
+    transaction:RefCell<Weak<Transaction>>
 }
 
 struct Transaction {
-    item: Vec<Item>,
+    item: RefCell<Vec<Item>>,
     amount: i64
 }
 
